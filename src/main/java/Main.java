98d3e60d01f630c88e1e088e9a62efe8182c262d
TimurGoghidze <105973783+TimurGoghidze@users.noreply.github.com>
2023-03-13 00:01:53 +0100

    add new Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "D:/2 tel run/chromedriver/chromedriver.exe");
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        WebElement searchBar = driver.findElement(By.cssSelector("[name='q']"));
+        searchBar.sendKeys("bicycle");
+        driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/form[1]/div[1]/div[1]/div[1]/div[1]/div[2]/input[1]"));
+        WebElement searchButton = driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[3]/form[1]/div[1]/div[1]/div[1]/div[1]/div[2]/input[1]"));
+
+        searchButton.click();
+    }
+
+}
