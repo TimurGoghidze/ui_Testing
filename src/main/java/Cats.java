ed5b396f66dbd1578dce9a3ab3e0063a6ff75e20
TimurGoghidze <105973783+TimurGoghidze@users.noreply.github.com>
2023-03-12 22:48:02 +0100

    add try catch

--- src/main/java/Cats.java
+++ src/main/java/Cats.java
@@ -1,8 +1,8 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -15,15 +15,21 @@ public class Cats {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "D:/2 tel run/chromedriver/chromedriver.exe"); //экземляр класса
         ChromeDriver driver = new ChromeDriver();//который позволяет нам взаимодействовать с браузером
-        String BASE_URL = "https://suninjuly.github.io/cats.html"; // хорошей практикой выводить отдельной переменной
-        driver.get(BASE_URL); //открываем драйвером нужную страницу
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));//не явное время ожидание т.е. для всех
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); //явное ожидание для конкретного элемента
-        WebElement header = driver.findElement(By.className("jumbotron-heading"));  //чтобы работать с вебэлементами и находить их
-        wait.until(ExpectedConditions.visibilityOf(header)); //ожидаем видимый header
-        // header.getText(); вернет текстовое значение которое внутри этого элемента
-        assertEquals("Cat memes", header.getText()); // dependencies junit
-        driver.quit();
+        try {
+            String BASE_URL = "https://suninjuly.github.io/cats.html"; // хорошей практикой выводить отдельной переменной
+            driver.get(BASE_URL); //открываем драйвером нужную страницу
+            //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));//не явное время ожидание т.е. для всех
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); //явное ожидание для конкретного элемента
+            WebElement header = driver.findElement(By.className("jumbotron-heading"));  //чтобы работать с вебэлементами и находить их
+            wait.until(ExpectedConditions.visibilityOf(header)); //ожидаем видимый header
+            String expectedHeaderText = "Dog memes";
+            assertEquals("This is not Elements does not contains text:" + expectedHeaderText,expectedHeaderText, header.getText()); // dependencies junit & header.getText(); вернет текстовое значение которое внутри этого элемента
+        }catch (AssertionError e){// перехват ошибки
+            e.printStackTrace(); // и выдача данных
+        } finally { // и по итогу
+            driver.quit();
+        }
+
     }
 
 }
