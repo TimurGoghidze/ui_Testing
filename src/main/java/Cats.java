c09b055f6f0dc9f0d958a559c6a6dd2de2650115
TimurGoghidze <105973783+TimurGoghidze@users.noreply.github.com>
2023-03-12 23:58:46 +0100

    add new Tests

--- src/main/java/Cats.java
+++ src/main/java/Cats.java
@@ -1,3 +1,6 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 
 import org.openqa.selenium.WebElement;
@@ -7,45 +10,40 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 public class Cats {
+    ChromeDriver driver; // чтобы передавался между методами и внизу ChromeDriver удаляем
+
+    @Before
     public void setUp() {// для порядка преобразуем так
         System.setProperty("webdriver.chrome.driver", "D:/2 tel run/chromedriver/chromedriver.exe"); //экземляр класса
-        ChromeDriver driver = new ChromeDriver();//который позволяет нам взаимодействовать с браузером
+        driver = new ChromeDriver();//который позволяет нам взаимодействовать с браузером
         String BASE_URL = "https://suninjuly.github.io/cats.html"; // хорошей практикой выводить отдельной переменной
         driver.get(BASE_URL); //открываем драйвером нужную страницу
     }
 
-    public void checkHeaderText(){
+    @Test
+    public void checkHeaderText() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); //явное ожидание для конкретного элемента
         WebElement header = driver.findElement(By.className("jumbotron-heading"));  //чтобы работать с вебэлементами и находить их
         wait.until(ExpectedConditions.visibilityOf(header)); //ожидаем видимый header
-        String expectedHeaderText = "Dog memes";
+        String expectedHeaderText = "Cat memes";
         assertEquals("This is not Elements does not contains text:" + expectedHeaderText, expectedHeaderText, header.getText()); // dependencies junit &
         // header.getText(); вернет текстовое значение которое внутри этого элемента
     }
-
-    public static void main(String[] args) {
-
-        try {
-            System.setProperty("webdriver.chrome.driver", "D:/2 tel run/chromedriver/chromedriver.exe"); //экземляр класса
-            ChromeDriver driver = new ChromeDriver();//который позволяет нам взаимодействовать с браузером
-            String BASE_URL = "https://suninjuly.github.io/cats.html"; // хорошей практикой выводить отдельной переменной
-            driver.get(BASE_URL); //открываем драйвером нужную страницу
-            //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));//не явное время ожидание т.е. для всех
-            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); //явное ожидание для конкретного элемента
-            WebElement header = driver.findElement(By.className("jumbotron-heading"));  //чтобы работать с вебэлементами и находить их
-            wait.until(ExpectedConditions.visibilityOf(header)); //ожидаем видимый header
-            String expectedHeaderText = "Dog memes";
-            assertEquals("This is not Elements does not contains text:" + expectedHeaderText, expectedHeaderText, header.getText()); // dependencies junit & header.getText(); вернет текстовое значение которое внутри этого элемента
-        } catch (AssertionError e) {// перехват ошибки
-            e.printStackTrace(); // и выдача данных
-        } finally { // и по итогу
-            driver.quit();
-        }
-
+ @Test
+ public void checkCatsCardsQuantity() {
+     List<WebElement> catsCards = driver.findElements(By.className("col-sm-4")); // нашли все карточки с котиками "s" большее кол-во
+     WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10)); // ждем 10 секутд
+     wait.until(ExpectedConditions.visibilityOf(catsCards.get(0))); //ожидаем  ожидаемые видимые карторчки с певой позиции т.е. 0
+     assertEquals("Quantity of cat cads is not 6!",6,catsCards.size()); // ожидаемо 6 штук
+ }
+    @After
+    public void tearDown() {
+        driver.quit();
     }
 
 }
