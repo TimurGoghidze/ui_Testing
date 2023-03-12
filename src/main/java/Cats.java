2f1d2886a375a9929567b7858c477a12d8177d5d
TimurGoghidze <105973783+TimurGoghidze@users.noreply.github.com>
2023-03-12 22:55:40 +0100

    add try catch

--- src/main/java/Cats.java
+++ src/main/java/Cats.java
@@ -11,11 +11,27 @@ import java.time.Duration;
 import static org.junit.Assert.assertEquals;
 
 public class Cats {
-
-    public static void main(String[] args) {
+    public void setUp() {// для порядка преобразуем так
         System.setProperty("webdriver.chrome.driver", "D:/2 tel run/chromedriver/chromedriver.exe"); //экземляр класса
         ChromeDriver driver = new ChromeDriver();//который позволяет нам взаимодействовать с браузером
+        String BASE_URL = "https://suninjuly.github.io/cats.html"; // хорошей практикой выводить отдельной переменной
+        driver.get(BASE_URL); //открываем драйвером нужную страницу
+    }
+
+    public void checkHeaderText(){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10)); //явное ожидание для конкретного элемента
+        WebElement header = driver.findElement(By.className("jumbotron-heading"));  //чтобы работать с вебэлементами и находить их
+        wait.until(ExpectedConditions.visibilityOf(header)); //ожидаем видимый header
+        String expectedHeaderText = "Dog memes";
+        assertEquals("This is not Elements does not contains text:" + expectedHeaderText, expectedHeaderText, header.getText()); // dependencies junit &
+        // header.getText(); вернет текстовое значение которое внутри этого элемента
+    }
+
+    public static void main(String[] args) {
+
         try {
+            System.setProperty("webdriver.chrome.driver", "D:/2 tel run/chromedriver/chromedriver.exe"); //экземляр класса
+            ChromeDriver driver = new ChromeDriver();//который позволяет нам взаимодействовать с браузером
             String BASE_URL = "https://suninjuly.github.io/cats.html"; // хорошей практикой выводить отдельной переменной
             driver.get(BASE_URL); //открываем драйвером нужную страницу
             //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));//не явное время ожидание т.е. для всех
@@ -23,8 +39,8 @@ public class Cats {
             WebElement header = driver.findElement(By.className("jumbotron-heading"));  //чтобы работать с вебэлементами и находить их
             wait.until(ExpectedConditions.visibilityOf(header)); //ожидаем видимый header
             String expectedHeaderText = "Dog memes";
-            assertEquals("This is not Elements does not contains text:" + expectedHeaderText,expectedHeaderText, header.getText()); // dependencies junit & header.getText(); вернет текстовое значение которое внутри этого элемента
-        }catch (AssertionError e){// перехват ошибки
+            assertEquals("This is not Elements does not contains text:" + expectedHeaderText, expectedHeaderText, header.getText()); // dependencies junit & header.getText(); вернет текстовое значение которое внутри этого элемента
+        } catch (AssertionError e) {// перехват ошибки
             e.printStackTrace(); // и выдача данных
         } finally { // и по итогу
             driver.quit();
