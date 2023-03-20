8b829057a12696c1ff89eeeed92bb1481500408b
TimurGoghidze <105973783+TimurGoghidze@users.noreply.github.com>
2023-03-20 21:35:13 +0100

    add FileUpload

--- src/main/java/DragAndDrop.java
+++ src/main/java/DragAndDrop.java
@@ -4,12 +4,16 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.nio.file.Watchable;
 import java.time.Duration;
 
+import static java.lang.Thread.sleep;
+import static org.junit.Assert.assertTrue;
+
 public class DragAndDrop {
-    ChromeDriver driver
+    ChromeDriver driver;
 
     @Before
     public void setUp() {
@@ -22,15 +26,22 @@ public class DragAndDrop {
     }
 
     @Test
-    public void dragAndDropTest(){
+    public void dragAndDropTest() throws InterruptedException {
         WebElement draggable = driver.findElement(By.id("draggable"));
         WebElement droppable = driver.findElement(By.id("droppable"));
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(draggable,droppable).perform(); //первый способ
+        // actions.moveToElement(draggable).clickAndHold().moveToElement(droppable).release().build().perform(); //второй способ
+        WebElement title = driver.findElement(By.id ("Dropped"));
+        assertTrue(title.getText().contains("Dropped"));
+
+
 
 
     }
 
     @After
-    public void tearDown(){
+    public void tearDown() {
         driver.quit();
     }
 }
